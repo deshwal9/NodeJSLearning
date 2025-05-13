9b3e58f82a1dd83c7af9b0944a63c722c5e51ed9
Ankit Kumar <kumaank2@in.ibm.com>
2025-05-13 01:32:01 +0100

    commting initail learing changes.

---
+++ VariableType.js
@@ -0,0 +1,14 @@
+let name = "max";
+var number = 29;
+var selectipon = false;
+// it cant be change becuase it defined as constant 
+const variable = "master"
+name =  "ajdas"
+function sumarisedUser(username, usernumber){
+     return ('Name is' + username + 'usernumber is' + usernumber)
+}
+
+console.log(name); 
+console.log(sumarisedUser(name, number)); 
+
+// console.log('hellow world from Node'); 
